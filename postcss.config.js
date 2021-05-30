dedc37d1ae532baeae48dbb89eb5acda453f0000
taneajoshi <taneajoshi@gmail.com>
2021-05-30 22:06:46 +0530

    final i guess

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+    plugins: [
+        require('autoprefixer'),
+        require('cssnano'),
+        // More postCSS modules here if needed
+    ]
+}
---
+++ src/js/main.js
@@ -0,0 +1,2 @@
+console.log('Hey just seeing if sass works fine!');
+import '../sass/app.scss';
